e1c85af69dd7ec73cfc699a7881b5724ca9c341c
Amy Martins <amy@amymartins.co.uk>
2012-04-23 20:48:13 +0000

    few changes

--- public/form-handler.php
+++ public/form-handler.php
@@ -1,12 +1,21 @@
 <?php
+ini_set('display_errors', 1);
 $name = $_POST["name"];
 $email = $_POST["email"];
-$comment = $_POST["message"];
+$comment = $_POST["comment"];
+$dsn = 'mysql:dbname=contact;host=127.0.0.1';
+$user = 'root';
+$password = 'password';
+$dbh = new PDO($dsn, $user, $password);
+$dbh->query("INSERT INTO `contact_request` (`name`, `email`, `comment`) VALUES ('$name', '$email', '$comment')");
 
-$formcontent = "From: $name \n comment: $comment";
-$recipient = "amy@amymartins.co.uk";
-$subject = "Contact Form";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
+//
+//$formcontent = "From: $name \n comment: $comment";
+//$recipient = "amy@amymartins.co.uk";
+//$subject = "Contact Form";
+//$mailheader = "From: $email \r\n";
+//$mailResult = mail($recipient, $subject, $formcontent, $mailheader); 
+//var_dump($mailResult);
+//        //or die("Error!");
+//echo "Thank You!";
 ?>
