079fad7ed6efd29856a0d2d315d80b7ea328baa3
Amy Martins <amy@amymartins.co.uk>
2012-05-05 20:39:13 +0000

    changes

--- public/calculator.php
+++ public/calculator.php
@@ -1,16 +1,19 @@
 <?php
+ini_set('diplay_errors', 1);
+ini_set('html_errors', 1);
 
+// var_dump($_GET);
+$first = $_GET["first"];
+$second = $_GET["second"];
+$op = $_GET["operator"];
 
+var_dump(first,second,op);
 ?>
 
-<html>
-    <head></head>
-    <body>
+
 <form>
-     type="text" name="first"
-     type="text" name="second"
-     type="text" name="operator"
-     input type="submit" name="submit" value="calculate"
-</form>
-    </body>
-</html>
+    <input type="text" name="first">
+    <input type="text" name="second">
+    <input type="text" name="operator">
+    <input type="submit" name="submit" value="calculate">
+</form?
