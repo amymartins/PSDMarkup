d91c222d69c59672c02493ae5f491944ed3b5f5d
Amy Martins <amy@amymartins.co.uk>
2012-05-05 20:55:21 +0000

    more changes

--- public/calculator.php
+++ public/calculator.php
@@ -3,11 +3,23 @@ ini_set('diplay_errors', 1);
 ini_set('html_errors', 1);
 
 // var_dump($_GET);
-$first = $_GET["first"];
-$second = $_GET["second"];
-$op = $_GET["operator"];
+//$first = $_GET["first"];
+//$second = $_GET["second"];
+// $op = $_GET["operator"];
 
-var_dump(first,second,op);
+//var_dump($first,$second,$op);
+
+if ($op == "-") {
+    echo "-";
+    } 
+    elseif ($op =="+") {
+        echo "+";
+    }
+    elseif ($op =="*") {
+        echo "*";
+    } elseif ($op =="/") {
+        echo "/";
+    }
 ?>
 
 
@@ -16,4 +28,4 @@ var_dump(first,second,op);
     <input type="text" name="second">
     <input type="text" name="operator">
     <input type="submit" name="submit" value="calculate">
-</form?
+</form>
